a71fac95d90c3bf8e056170da23c4968ab74c322
DiogoLogic <diogologic@gmail.com>
2024-07-27 19:35:41 -0300

    atributo data adcionado

--- InventoryWarehouse/src/loja/Vendedor.java
+++ InventoryWarehouse/src/loja/Vendedor.java
@@ -1,9 +1,13 @@
 package loja;
 
+import java.sql.Date;
+
 public class Vendedor {
 	
 	private String nome;
 	
+	private Date dataNascimento;
+
 	private String cpf;
 	
 	private Integer idade;
@@ -15,6 +19,17 @@ public class Vendedor {
 		this.idade = idade;
 	}
 	
+	
+	
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
 	
 	public Integer getIdade() {
 		return idade;
